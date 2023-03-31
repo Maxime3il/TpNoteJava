d7f165634c88fd3fae267746b4a3c9762456c0cc
maxime barbaste <m.barbaste@gavap.com>
2023-03-31 09:11:26 +0200

    mise en forme générale de l'application, background/logo/controleur graphique

--- GestionnaireCouleurs/src/application/Main.java
+++ GestionnaireCouleurs/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -12,9 +13,15 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("IHM.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,500,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			// Titre de la fenêtre
+			primaryStage.setTitle("Gestionnaire de couleurs");
+			// Logo 3il de la fenêtre récupérable via le dossier Images
+			primaryStage.getIcons().add(new Image(ClassLoader.getSystemResourceAsStream("Images/3ilLogo.jpg")));
+			// Impossible à l'utilisateur de modifier la taille et le dimensions de la fenêtre
+            primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
