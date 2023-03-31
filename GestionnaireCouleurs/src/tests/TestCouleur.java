d97b39758a1e185ec348b0c03bccdcd59cbd1fa4
maxime barbaste <m.barbaste@gavap.com>
2023-03-31 09:48:52 +0200

    Test de l'accesseur bleu du rgb echec

--- GestionnaireCouleurs/src/tests/TestCouleur.java
+++ GestionnaireCouleurs/src/tests/TestCouleur.java
@@ -14,12 +14,13 @@ class TestCouleur {
 		Couleur couleurTest = new Couleur("rouge", 255, 0, 0);
 		//Test du nom de la couleur
 		Assertions.assertEquals("rouge", couleurTest.getNom());
-		//Test d'accesseur pour la rgb rouge 
+		//Test d'accesseur pour le rgb rouge 
 		Assertions.assertEquals(255, couleurTest.getRouge());
-		//Test d'accesseur pour la rgb verte 
+		//Test d'accesseur pour le rgb verte 
 		Assertions.assertEquals(0, couleurTest.getVert());
+		//Test d'accesseur pour le rgb bleu 
+		Assertions.assertEquals(0, couleurTest.getBleu());
 
-		
 	}
 
 }
