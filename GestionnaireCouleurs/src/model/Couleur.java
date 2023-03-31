9d6424603782d852f2c05608175502f03da74d9a
maxime barbaste <m.barbaste@gavap.com>
2023-03-31 09:37:39 +0200

    Test instanciation Constructeur Couleur avec arguments réussi

--- GestionnaireCouleurs/src/model/Couleur.java
+++ GestionnaireCouleurs/src/model/Couleur.java
@@ -2,4 +2,16 @@ package model;
 
 public class Couleur {
 
+	private String nom;
+	private int rouge;
+	private int vert;
+	private int bleu;
+	
+	public Couleur(String nom, int rouge, int vert, int bleu) {
+		this.nom = nom;
+		this.rouge = rouge;
+		this.vert = vert;
+		this.bleu = bleu;
+	}
+
 }
